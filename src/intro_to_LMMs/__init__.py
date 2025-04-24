79f19c3e82e3443bb65738fe722d756b7ad10191
Soumya Banerjee <neel.soumya@gmail.com>
2025-04-24 11:40:51 +0000

    ed

---
+++ src/intro_to_LMMs/__init__.py
@@ -0,0 +1,11 @@
+"""
+intro_to_LMMs: A package for exploring large language models (LMMs).
+"""
+
+__version__ = "0.1.0"
+
+def greet():
+    """
+    A simple function to demonstrate the package functionality.
+    """
+    return "Welcome to the intro_to_LMMs package!"
